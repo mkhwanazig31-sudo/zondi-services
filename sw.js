dd784401cd524b9b332667927ac90f7b95305eaf
mkhwanazig31-sudo <mkhwanazig31@gmail.com>
2026-09-22 23:54:32 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,2 +1,3 @@
-self.addEventListener('install', e=>{e.waitUntil(caches.open('zondi-v1').then(c=>c.addAll(['/','/shop','/login','/clients','/patrollers','/sos'])));});
-self.addEventListener('fetch', e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
+self.addEventListener('install', e => self.skipWaiting());
+self.addEventListener('activate', e => self.clients.claim());
+self.addEventListener('fetch', e => e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
